68dc58ca0e2a757b7db44ca5b7a84ae01c1f6aed
fmvasilenko <62238648+fmvasilenko@users.noreply.github.com>
2021-02-09 11:32:30 +0300

    started Firebase/Storage

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,4 +1,13 @@
 import ToolsPanel from '../shared/view/components/ToolsPanel/ToolsPanel';
+// import Storage from '../shared/services/DB/Databases/Firebase/Storage';
+
+// const DB = new Storage();
+
+/*
+DB.saveTable('event', 'user', [['free', 'busy', 'preferred'], ['undesirable', 'free', 'busy']])
+  .then(() => console.log('success'))
+  .catch((error) => console.log(error));
+  */
 
 const Main = (): JSX.Element => {
   return (
